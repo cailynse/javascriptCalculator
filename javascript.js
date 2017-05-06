37709365f964dddbaec13f5ff549ec31bdaae27b
Cailyn <cailyn.s.e@gmail.com>
2017-05-06 18:30:52 -0400

    Basic Outline
    I've created a basic calculator and styled it simply. Now I am starting to work on functionality. I'm following an example online and trying to make improvments as I go.

---
+++ javascript.js
@@ -0,0 +1,28 @@
+(function () {
+
+    var memory = "0",
+        currChar = "0",
+        operation = 0,
+        maxLength = 20;
+
+    function addChar(x) {
+        if (currChar.length > maxLength) {
+
+        }
+    }
+
+    function addOper(y) {
+
+    }
+
+    function doMath() {
+
+    }
+
+    function clear() {
+
+    }
+
+
+
+})();
